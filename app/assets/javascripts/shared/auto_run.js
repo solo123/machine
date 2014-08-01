e8fd4f300d960314cc6010d279bd3b314b80ca51
Jimmy Liang <jimmy.liang@gmail.com>
2014-08-01 15:43:30 +0800

    js sum

--- app/assets/javascripts/shared/auto_run.js
+++ app/assets/javascripts/shared/auto_run.js
@@ -1,3 +1,31 @@
 $(function(){
+  $('.best_in_place').bind("ajax:success", function () { sum_all_tables(); });
   $(".best_in_place").best_in_place();
+
+  sum_all_tables();
 });
+
+var test_obj = null;
+
+function sum_all_tables(){
+  $('table.sum-table').each(function(){ sum_table($(this));});
+}
+
+function sum_table(table){
+   table.find('[class*=sum_]').each(function() { sum_column($(this), table); });
+}
+
+function sum_column(sum_cell, table){
+  $.each(sum_cell.attr('class').split(' '), function(i, val){
+    var re = val.match(/^sum_(.*)/);
+    if(re){
+      var tot = 0.0;
+      table.find('.' + re[1]).each(function(){
+        tot += parseFloat($(this).text());
+      });
+
+      sum_cell.text(tot);
+    }
+  });
+
+}
