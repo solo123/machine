2726e583565ce644d37550f39bb98ac4d811c016
Jimmy Liang <jimmy.liang@gmail.com>
2014-11-28 10:39:21 +0800

    errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,7 @@
 Airbrake.configure do |config|
   config.api_key = '39595933c52d18b9c5ca1e2f68c5a996'
-  config.host    = 'wooul.com'
+  config.host    = '122.112.89.151'
   config.port    = 3001
   config.secure  = config.port == 443
+  config.development_environments = []
 end
